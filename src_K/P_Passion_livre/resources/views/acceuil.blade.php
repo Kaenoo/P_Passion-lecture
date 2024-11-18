9b6cbd803fe3d2193904e021f1774d8bd9ce0ead
Kaeno Louis Eyer <pg43msk@eduvaud.ch>
2024-11-18 14:46:49 +0100

    LARAVEL + TAILWIND

--- src_K/P_Passion_livre/resources/views/acceuil.blade.php
+++ src_K/P_Passion_livre/resources/views/acceuil.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil - Passion lecture</title>
+    @vite('resources/css/app.css')
+    <title>Document</title>
 </head>
 <body>
-    
+    <h1>
+        hello world!
+    </h1>
 </body>
 </html>

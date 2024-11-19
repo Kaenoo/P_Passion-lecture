13e96c425e62bca8b7f06dfc41cf588f5185a4ec
Kaeno Louis Eyer <pg43msk@eduvaud.ch>
2024-11-19 17:10:14 +0100

    progression index.php

--- src/index.php
+++ src/index.php
@@ -6,9 +6,23 @@
     <link rel="stylesheet" href="../css/output.css">
     <title>Accueil - Passion lecture</title>
 </head>
-<body>
-    <header class="flex w-full bg-green-700 py-6">
-      <h1 class="text-3xl font-bold ">Passion Livre</h1>
+<body class="m-auto w-full">
+    <header class="flex  bg-green-700 py-6 px-4">
+      <a href="./index.php">
+        <h1 class="text-3xl font-bold">Passion Livre</h1>
+      </a>
+
+      <div class="flex content-around">
+        <a href="./addbook.php">
+          <img src="img/addBook.png" alt="">
+        </a>
+        <a href="./booklist.php">
+          <img src="img/book_list.png" alt="">
+        </a>
+        <a href="./login.php">
+          <img src="img/Account.png" alt="">
+        </a>
+      </div>
     </header>
 
     <body>

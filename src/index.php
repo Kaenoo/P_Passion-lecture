bc45ed6728057baa3e6323da3a677b59d9ddcc28
Kaeno Louis Eyer <pg43msk@eduvaud.ch>
2024-11-19 13:29:51 +0100

    Début index.php

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/output.css">
+    <title>Accueil - Passion lecture</title>
+</head>
+<body>
+    <header class="flex w-full bg-green-700 py-6">
+      <h1 class="text-3xl font-bold ">Passion Livre</h1>
+    </header>
+
+    <body>
+        
+    </body>
+</body>
+</html>

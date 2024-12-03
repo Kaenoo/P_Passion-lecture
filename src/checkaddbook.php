eacc1c4208489dc3a39bc8d036de3bf432561d01
mus7afayildiz <mus7afayildiz@yandex.com>
2024-12-03 10:36:38 +0100

    feat: Permet d'ajouter un livre à la base de données

--- src/checkaddbook.php
+++ src/checkaddbook.php
@@ -5,14 +5,11 @@
  * Description  : Le page pour ajouter un livre
  */
 session_start();
-include ("./models/Database.php");
+include("./models/Database.php");
 $db = new Database();
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
 
-var_dump($_SESSION);
-die;
-$db->addBook($_POST, $_FILES, $_SESSION["userID"]);
+// Permet de ajouter un livre
+$db->addBook($_POST, $_FILES, $_SESSION['user']['userID']);
 
+// Aller au page accueil après ajouter un livre
 header("location: ./index.php");

06c58c5396cfe7ec457c1ed5b62768cf5a3c9a6c
unknown <mus7afayildiz@yandex.com>
2024-12-27 21:19:47 +0100

    feat: ajouter editBook.php

--- src/controllers/checkEditBook.php
+++ src/controllers/checkEditBook.php
@@ -2,10 +2,15 @@
 /*
  * Auteur       : Mustafa Yildiz
  * Date         : 19.11.2024
- * Description  : Le page pour ajouter un livre
+ * Description  : Le page pour contrôlere de editer un livre
  */
 session_start();
 include("C:\Users\Mustafa\Desktop\Projets\P_Passion-lecture\src\models\Database.php");
+
+// if (!$this->isUserAuthorized($userID, $id_ouvrage)) {
+//     throw new RuntimeException("Vous n'êtes pas autorisé à mettre à jour ce livre.");
+// };
+
 echo "<pre>";
 var_dump($_POST);
 echo "</pre>";
@@ -21,14 +26,14 @@ $db = new Database();
 
 
 // Permet de ajouter un livre
-$db->addBook($_POST, $_FILES, $_SESSION['user']['userID']);
+$db->editBook($_POST, $_FILES, $_SESSION['user']['userID']);
 
 
 // Permet de contôler fichier
 
 
 
-  
+
 
 // Aller au page accueil après ajouter un livre
 header("location: ../index.php");

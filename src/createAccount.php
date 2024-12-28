970d0122755eac0e68ae0173c2e48e8e84e1dcdc
unknown <pg43msk@eduvaud.ch>
2024-12-28 19:33:14 +0100

    Feat : Ajout double entrée du mdp lors de la création d'un compte

--- src/createAccount.php
+++ src/createAccount.php
@@ -11,24 +11,31 @@ $db = new Database();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && count($_POST) > 1) {
 
+  var_dump($_POST);
     // Vérifie si le pseudo existe dans la db, si ce n'est pas le cas -> Création de compte
     if ($db->verifyPseudoExistence($_POST["pseudo"]) !== true) {
       
-      //Préparation des données
-      $date = date("Y-m-d");
-      $password = password_hash($_POST["pseudo"], PASSWORD_DEFAULT);
-
-      $db->CreateAccount($_POST["lastName"], $_POST["firstName"], $_POST["pseudo"], $password, $date);
-      
-      // Récupère l'id de l'user
-      $userData = $db->getDataAccount($_POST["pseudo"]);
-      $idUser = $userData["utilisateur_id"];
-      
-      //Créer une session au nouvel user
-      getConnectedUser(0, $idUser);
+      // Vérifie si l'user ne s'est pas trompé dans l'entrée des mots de passe
+      if ($_POST["password"] === $_POST["password2"]) {
+        //Préparation des données
+        $date = date("Y-m-d");
+        $password = password_hash($_POST["pseudo"], PASSWORD_DEFAULT);
+  
+        $db->CreateAccount($_POST["lastName"], $_POST["firstName"], $_POST["pseudo"], $password, $date);
+        
+        // Récupère l'id de l'user
+        $userData = $db->getDataAccount($_POST["pseudo"]);
+        $idUser = $userData["utilisateur_id"];
+        
+        //Créer une session au nouvel user
+        getConnectedUser(0, $idUser);
+      }
+      else {
+        $error = 'Les mots de passe entrés sont différents !';
+      }
     }
     else {
-      echo "Ce pseudo existe déjà, prends-en un autre !";
+      $error = "Ce pseudo existe déjà, prends-en un autre !";
     }
 }
 
@@ -45,10 +52,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && count($_POST) > 1) {
 <body class="h-full">
 <?php include("./views/header.php"); ?>
 
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<div class="flex min-h-full flex-col justify-center px-6 py-8 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <!-- <img class="mx-auto h-10 w-auto" src="./img/account.png" alt=""> -->
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Créer un compte</h2>
+    <h2 class="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Créer un compte</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -85,6 +92,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && count($_POST) > 1) {
         </div>
       </div>
 
+      <!-- Confirmation du mot de passe -->
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm/6 font-medium text-gray-900">Confirmation du mot de passe</label>
+        </div>
+        <div class="mt-2">
+          <input id="password2" name="password2" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-700 sm:text-sm/6">
+        </div>
+      </div>
+
+      <!-- En cas d'erreur, le message s'affiche ici -->
+      <p class="text-center font-semibold text-red-700"><?php
+          if (isset($error)) {
+            echo $error;
+          }
+        ?></p>
+
+      <!-- Validation du compte -->
       <div>
         <button type="submit" class="flex w-full justify-center rounded-md bg-green-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700">Créer un compte</button>
       </div>
--- src/login.php
+++ src/login.php
@@ -44,10 +44,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && count($_POST) > 1) {
 include("./views/header.php");
 ?>
 
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<div class="flex min-h-full flex-col justify-center px-6 py-8 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <!-- <img class="mx-auto h-10 w-auto" src="./img/account.png" alt=""> -->
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Connecte toi à ton compte</h2>
+    <h2 class="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Connecte toi à ton compte</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -70,6 +70,7 @@ include("./views/header.php");
         </div>
       </div>
         
+        <!-- En cas d'erreur, le message s'affiche ici -->
         <p class="text-center font-semibold text-red-700"><?php
           if (isset($error)) {
             echo $error;

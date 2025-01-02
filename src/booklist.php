e51f075af29f57642d7d9c71367b109ae528a5c8
CielRii <sarah.dongmo@eduvaud.ch>
2025-01-02 11:27:53 +0100

    Feat : Ajout données affichables du livre, Gestion changement de page ; Fix : Élément de recherche  toujours visible dans la barre de recherche après recherche, Mise au propre du code

--- src/booklist.php
+++ src/booklist.php
@@ -6,26 +6,15 @@
   session_start();
   include("./models/database.php");
   include("./controllers/books.php");
-  $booksController = new booksController();
   $db = new Database();
+  $booksController = new booksController();
 
-  $limit = 2;
+  $limit = 5;
   $page = 4;
-
-  if ($_GET["search"]) 
-  {
-    //var_dump($_GET);
-    //var_dump($_GET[1]);
-    //[0]["nom"]
-    $listBooks = $db->searchBooks($_GET);
-    $nbOfBooks = $db->getRowsNumberOfSearch($_GET);
-  }
-  else
-  {
-    $listBooks = $db->listBooks();
-    $nbOfBooks = $db->getRowsNumberOfList();
-  }
-
+  $disabled = 0;
+  $nbOfPages;
+  $pagination = 0;
+  $page = 1;
 ?>
 
 
@@ -45,21 +34,6 @@
     <main>
       <!-- Lien pour revenir sur l'affichage par défaut -->
       <h1 class="py-4 text-4xl font-bold text-center"> <a href="bookList.php">Liste des ouvrages </a></h1>
-      <!-- <div class="search-container">
-        <form action="" method="get">
-          <input type="text" placeholder="Search.." id="search" name="search" oninput="myFunction()">
-          <button type="submit"><i class="fa fa-search"></i></button>
-        </form>
-      </div>
-
-      <script>
-      function myFunction() 
-      {
-        $searchValue = '"document.getElementById("search").value'; 
-        $db->searchABook($searchValue);?>
-      }
-      </script>  -->
-
 
       <!-- Barre de recherche -->
       <form action="#" method="get">
@@ -67,18 +41,23 @@
             <div class="join">
                 <div>
                 <div>
-                  <input class="input input-bordered join-item" placeholder="Recherche" id="search" name="search"/>
+                  <input class="input input-bordered join-item" 
+                  <?php if (isset($_GET['search']) || isset($_GET['categorie'])){ echo 'value="' . $_GET['search']. '"';}?>
+                  placeholder = "Recherche" id="search" name="search"/>  
                 </div>
               </div>
-              <select id="categories" name="categories" class="select select-bordered join-item">
-                <option value="filter" selected>Filtre</option>
+              <select id="categorie" name="categorie" class="select select-bordered join-item">
+                <option value="Filtre" selected>
+                <?php  if (isset($_GET['search']) || isset($_GET['categorie'])){echo $_GET['categorie'];} else {echo "Filtre";} ?>
+                </option>
                 <!-- Affichage dynamique des catégories -->
                 <?php 
                     $categories = $booksController->categories($db);
-                    foreach ($categories as $key => $value) {
+                    foreach ($categories as $key => $value) 
+                    {
                       echo '<option value="'. $value . '">' . $value . '</option>';
                     }
-                    ?>
+                ?>
               </select>
               <div class="indicator">
                 <button type="submit" class="btn join-item bg-green-700 hover:bg-green-600 font-bold">Rechercher</button>
@@ -88,78 +67,126 @@
       </form>
     
     <!-- Recherche des livres disponibles dans la base de données -->
-    <?php      
-      $count = 0;
-        
-      //switch ()
+    <?php
+    
+      // Requête vers la base de données pour connaître le nombre de livres selon si une recherche a été effectuée ou pas
+      if (isset($_GET['search']) || isset($_GET['categorie'])) 
+      {
+        $nbOfBooks = $db->getRowsNumberOfSearch($_GET);
+      }
+      else
+      {
+        $nbOfBooks = $db->getRowsNumberOfList();
+      }
+
+      // Définition du nombre de page
+      $nbOfPages = $nbOfBooks/$limit;
+      if ($nbOfBooks % $limit != 0)
+      { 
+        $nbOfPages ++;
+        $nbOfPages = floor($nbOfPages); //Arrondi à l'entier inférieur
+      }
+
+      // Mise à jour de l'état de pagination
+      if ($nbOfPages > 1)
+      {
+        $pagination = 1;
+      }
+
+      // Calcul de l'index de la valeur de la requête
+      if (isset($_GET['page'])) 
+      {
+        $page = $_GET['page'];
+      }
+      $index = (($limit * $page) - $limit);
+      if ($page != 1) {$index++;}
+
+      // Requête vers la base de données selon si une recherche est effectuée ou non
+      if (isset($_GET['search']) || isset($_GET['categorie'])) 
+      {
+        $listBooks = $db->searchBooks($_GET, $index, $limit);
+      }
+      else
+      {
+        $listBooks = $db->listBooks($index, $limit);
+      }
+
         foreach ($listBooks as $dataBook)
         { 
-
           echo "<div class = 'globalDataBook'>";
-
-            echo "<tr>";
-            echo "<td>";
-            echo "<div class='cat'>";
-            echo '<img src="'.$dataBook["image_couverture"].'" alt="Couverture du livre">';
-            echo "</div>";
-            echo "</td>";
+          echo "<div class='cat'>";
+          echo '<img src="'.$dataBook["image_couverture"].'" alt="Couverture du livre">';
+          echo "</div>";
 
             echo "<div class='textDataBook'>";
               $listPseudoUser = $db->listPseudoUser ($dataBook["utilisateur_id"]);
               foreach ($listPseudoUser as $pseudoUser)
               {
-                echo "<tr>";
-                echo "<td>";
                 echo "<div class='pseudo'>";
-                echo '<a href="myBooks.php?id='. $pseudoUser["utilisateur_id"] .'">' . $pseudoUser["pseudo"] . "</a>" . "<br> ";
+                echo '<a href="userBooks.php?id='. $pseudoUser["utilisateur_id"] .'">' . $pseudoUser["pseudo"] . "</a>" . "<br> ";
                 echo "</div>";
-                echo "</td>";
               }
-
-              echo "<div class='directDataBook'>";
-                echo "<tr>";
-                echo "<td>";
-                echo '<a href="publisherData.php?id='. $dataBook["ouvrage_id"] .'">' . $dataBook["titre"] . "</a>" . ", ";
-                echo "</td>";
+                echo "<div class='directDataBook'>";
+                echo '<a href="book.php?id='. $dataBook["ouvrage_id"] .'">' . $dataBook["titre"] . "</a>" . ", ";
                 
                 $listAuthorBook = $db->listAuthorBook($dataBook["ecrivain_id"]);
                 foreach ($listAuthorBook as $authorBook)
                 {
-                  echo "<tr>";
-                  echo "<td>";
                   echo $authorBook["prenom"] . " ";
                   echo $authorBook["nom"] . "</br>";
-                  echo "</td>";
                 }
-              echo "</div>";
+              
               $listCategoryBook = $db->listCategoryBook ($dataBook["categorie_id"]);
               foreach ($listCategoryBook as $categoryBook)
               {
-                echo "<tr>";
-                echo "<td>";
-                echo $categoryBook["nom"] . " ";
-                echo "</td>";
+                echo $categoryBook["nom"] . "</br>";
               }
-            echo "</div>";
 
-            echo "</tr>";
+              $listSummaryBook = $db->listSummaryBook ($dataBook["ouvrage_id"]);
+              //var_dump($listSummaryBook);
+              foreach ($listSummaryBook as $summaryBook)
+              {
+                echo $summaryBook["resume"] . "</br>";
+              }
           echo "</div>";
-          $count ++;
-
-          if ($count == $limit)
-          {
-            $page++;
-            break;
-          }
+          echo "</div>";
+          echo "</div>";
         }
 
-        
-        if ($nbOfBooks > $limit)
+
+        // Pagination adaptée au nombre de livres
+        if ($nbOfBooks > $limit) //pagination == 1
         {
-          echo '<div class="join grid grid-cols-2">
-          <a class="join-item btn btn-outline" href="bookList.php?id=1">Page précédente</a>
-          <a class="join-item btn btn-outline" href="bookList.php?id='. $page. '">Suivante</a>
-          </div>';
+          $i = 1;
+          echo '<div class="join">
+          <input class="join-item btn btn-square " type="radio" name="options" onclick="window.location.href=\'bookList.php?page=' . ($page > 1 ? $page - 1 : $page) .'\';"aria-label="&laquo;" />';
+
+          for ($i; $i <= $nbOfPages; $i++)
+          {
+            // Identification de la page active
+            if (isset($_GET["page"])) 
+            { $currentPage = $_GET["page"]; } 
+            else 
+            { $currentPage = 1;}
+          
+            // Indexation de la page active
+            if ($i == $currentPage)
+            { $checked = 'checked="checked"'; }
+            else 
+            { $checked = '';}
+
+            if ($i == 1)
+            {
+              echo '<input class="join-item btn btn-square" type="radio" name="options" 
+              onclick="window.location.href=\'bookList.php?page=' . $i . '\';" aria-label="' . $i . '" ' . $checked . '/>'; 
+            }
+            else
+            {
+              echo '<input class="join-item btn btn-square" type="radio" name="options" onclick="window.location.href=\'bookList.php?page=' . $i . '\';"
+              aria-label="' . $i . '"/>';
+            }
+          }
+          echo '<input class="join-item btn btn-square" type="radio" name="options" onclick="window.location.href=\'bookList.php?page=' . ($page < $nbOfPages ? $page + 1 : $page). '\';"aria-label="&raquo;" />';
         }
   
       ?>
@@ -170,10 +197,14 @@
 <style>
    .globalDataBook {
           display : flex;
-          /* align-items : center;
+          /*align-items : center;
           justify-content: center;*/
-          margin-left: 30%;
-          margin-right: 30%;
+          /*margin-left: 30%;
+          margin-right: 30%;*/
+          width: 40%; /* Définit la largeur de l'élément à 40% de la largeur du conteneur parent */
+      margin-left: auto; /* Centrer l'élément horizontalement */
+    margin-right: auto; /* Centrer l'élément horizontalement */
+    text-align : center;
         }
 
         img {
@@ -186,7 +217,7 @@
         height:300px;
         width: 300px;
         margin-bottom: 2%;
-        object-fit: cover;
+        /*object-fit: cover;*/
         }
 
         .textDataBook
@@ -203,7 +234,7 @@
       .directDataBook{
         display : flex;
         flex-direction: row;
-        font-size: 25px;
+        font-size: clamp(1rem, 3vw, 2rem);
 
       }
 

a1e4d1bc8f4eb05dce39ad8c12c1db905b87a41e
unknown <pg43msk@eduvaud.ch>
2025-01-02 17:30:53 +0100

    UX(index.php): responsive design

--- src/index.php
+++ src/index.php
@@ -29,12 +29,12 @@ if (isset($_GET["login"]) && $_GET["login"] === "out") {
 <body class="m-auto w-full">
   <?php include("./views/header.php"); ?>
 
-  <main class="px-6 md:px-12 text-justify">
+  <main class="px-6 md:px-12 text-start">
     <h1 class="my-4 text-3xl md:text-4xl font-bold text-center">Accueil</h1>
 
     <!-- Message d'introduction -->
-    <div class="m-5 p-5 bg-green-200 ">
-      <p>Bienvenue sur Passion Lecture, votre espace dédié à la découverte et au partage littéraire ! Que vous soyez un amateur de romans, un passionné d’essais ou un curieux de récits historiques, ce site vous permet :</p>
+    <div class="lg:mx-16 my-10 lg:p-16 p-6 text-lg lg:text-2xl leading-7 md:leading-9 bg-green-200">
+      <p>Bienvenue sur Passion Lecture, votre espace dédié à la découverte et au partage littéraire! Que vous soyez un amateur de romans, un passionné d’essais ou un curieux de récits historiques, ce site vous permet :</p>
 
       <ul class="list-disc pl-8">
         <li>Explorer de nouvelles lectures : Parcourez une vaste bibliothèque d’ouvrages classés par catégories, genres, ou auteurs.</li>
@@ -42,8 +42,7 @@ if (isset($_GET["login"]) && $_GET["login"] === "out") {
         <li>Découvrir des recommandations personnalisées : Trouvez votre prochaine lecture grâce à nos suggestions basées sur vos préférences.</li>
         <li>Suivre vos auteurs favoris : Restez informé des œuvres de vos écrivains préférés.</li>
       </ul>
-      <p>Les cinq derniers ouvrages ajoutés sont disponibles directement sur cette page pour une exploration immédiate. Bonne lecture ! 📚 <br>
-      Si vous avez des précisions à apporter ou souhaitez une version plus ciblée, faites-le-moi savoir !</p>
+      <p>Les cinq derniers ouvrages ajoutés sont disponibles directement sur cette page pour une exploration immédiate. Bonne lecture ! 📚</p>
     </div>
 
     <!-- 5 derniers ouvrages -->
